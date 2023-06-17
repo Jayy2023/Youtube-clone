a12073108be73344bf301f38b4d848d4da69286e
Jayy2023 <119895456+Jayy2023@users.noreply.github.com>
2023-06-17 13:46:58 -0400

    adding video cards to recommended section

--- src/components/RecommendedVideos.jsx
+++ src/components/RecommendedVideos.jsx
@@ -1,10 +1,109 @@
 import React from 'react'
 import './RecommendedVideos.css'
+import VideoCard from './VideoCard'
 
 export default function RecommendedVideos() {
   return (
     <div className='recommendedVideos'>
        <h2>Recommended</h2>
+       <div className="recommendedVideos__videos">
+        <VideoCard 
+            title="Russell Westbrooks BEST 2023 Playoffs Highlights So Far! 🔥"
+            views="12K views"
+            timestamp="1 month ago"
+            channelImage="images/jordanTesla.jpg"
+            channel="Jordan Couch"
+            image="images/RussellWestbrook.jpeg"
+        />
+        <VideoCard 
+            title="Nevaeh Couch First Grade Basketball 2022-23 🔥"
+            views="1.2M views"
+            timestamp="3 months ago"
+            channelImage="images/jordanTesla.jpg"
+            channel="Jordan Couch"
+            image="images/NevaehBall.JPG"
+        />
+        <VideoCard 
+            title="Jordan Couch Senior Year Basketball 2015-16 🤷🏾 🔥"
+            views="13k views"
+            timestamp="5 minutes ago"
+            channelImage="images/jordanTesla.jpg"
+            channel="Jordan Couch"
+            image="images/JordanBall.JPG"
+        />
+        <VideoCard 
+            title="Russell Westbrooks BEST 2023 Playoffs Highlights So Far! 🔥"
+            views="12K views"
+            timestamp="1 month ago"
+            channelImage="images/jordanTesla.jpg"
+            channel="Jordan Couch"
+            image="images/RussellWestbrook.jpeg"
+        />
+        <VideoCard 
+            title="Nevaeh Couch First Grade Basketball 2022-23 🔥"
+            views="1.2M views"
+            timestamp="3 months ago"
+            channelImage="images/jordanTesla.jpg"
+            channel="Jordan Couch"
+            image="images/NevaehBall.JPG"
+        />
+        <VideoCard 
+            title="Jordan Couch Senior Year Basketball 2015-16 🤷🏾 🔥"
+            views="13k views"
+            timestamp="5 minutes ago"
+            channelImage="images/jordanTesla.jpg"
+            channel="Jordan Couch"
+            image="images/JordanBall.JPG"
+        />
+        <VideoCard 
+            title="Russell Westbrooks BEST 2023 Playoffs Highlights So Far! 🔥"
+            views="12K views"
+            timestamp="1 month ago"
+            channelImage="images/jordanTesla.jpg"
+            channel="Jordan Couch"
+            image="images/RussellWestbrook.jpeg"
+        />
+        <VideoCard 
+            title="Nevaeh Couch First Grade Basketball 2022-23 🔥"
+            views="1.2M views"
+            timestamp="3 months ago"
+            channelImage="images/jordanTesla.jpg"
+            channel="Jordan Couch"
+            image="images/NevaehBall.JPG"
+        />
+        <VideoCard 
+            title="Jordan Couch Senior Year Basketball 2015-16 🤷🏾 🔥"
+            views="13k views"
+            timestamp="5 minutes ago"
+            channelImage="images/jordanTesla.jpg"
+            channel="Jordan Couch"
+            image="images/JordanBall.JPG"
+        />
+        <VideoCard 
+            title="Russell Westbrooks BEST 2023 Playoffs Highlights So Far! 🔥"
+            views="12K views"
+            timestamp="1 month ago"
+            channelImage="images/jordanTesla.jpg"
+            channel="Jordan Couch"
+            image="images/RussellWestbrook.jpeg"
+        />
+        <VideoCard 
+            title="Nevaeh Couch First Grade Basketball 2022-23 🔥"
+            views="1.2M views"
+            timestamp="3 months ago"
+            channelImage="images/jordanTesla.jpg"
+            channel="Jordan Couch"
+            image="images/NevaehBall.JPG"
+        />
+        <VideoCard 
+            title="Jordan Couch Senior Year Basketball 2015-16 🤷🏾 🔥"
+            views="13k views"
+            timestamp="5 minutes ago"
+            channelImage="images/jordanTesla.jpg"
+            channel="Jordan Couch"
+            image="images/JordanBall.JPG"
+        />
+       </div>
     </div>
   )
 }
